827507d5a37ddf755a38795d87cc7b32d8963008
osvaldosilitonga <osvaldosilitonga@gmail.com>
2023-09-25 11:12:59 +0700

    Task 3

---
+++ code3/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number int
+
+	fmt.Print("Please enter a number : ")
+	fmt.Scan(&number)
+
+	if number < 1 {
+		fmt.Println("Angka harus lebih besar dari 0")
+	}
+
+	if number%2 == 0 {
+		fmt.Println("The number is even.")
+	} else {
+		fmt.Println("The number is odd.")
+	}
+
+	fmt.Println("Even numbers upto your input are :")
+
+	for i := 1; i <= number; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	if number == 1 {
+		fmt.Println("Bilangan prima harus lebih atau sama dengan 2")
+	}
+
+	isPrime := false
+	for x := 2; x <= number; x++ {
+		if x == number {
+			isPrime = true
+			break
+		}
+		if number%x == 0 {
+			isPrime = false
+			break
+		}
+	}
+
+	if isPrime == true {
+		fmt.Println("The number is a prime number")
+	} else {
+		fmt.Println("The number is not a prime number")
+	}
+
+}
